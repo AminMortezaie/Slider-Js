f67786d0ee3151fad312836318593fe5e5e5554a
AminMortezaie <mohammadmortezaie@gmail.com>
2019-07-30 01:12:37 +0430

    Make HTML file without inner js codes.
    All the js codes is in external js file.

--- slider.js
+++ slider.js
@@ -1,6 +1,23 @@
 var slideIndex = 0;
 var timer = null;
+
+var slides = document.getElementsByClassName("mySlides");
+createSpan();
 showSlides();
+
+
+function createSpan() {
+    var parent = document.getElementsByClassName("mySpan")[0];
+    var child;
+    for (let i = 0; i < slides.length; i++) {
+        child = document.createElement("span");
+        child.setAttribute("class", "dot");
+        child.addEventListener("click", () => {
+            currentSlide(i + 1);
+        });
+        parent.appendChild(child);
+    }
+}
 
 
 function plusSlides(n) {
@@ -15,10 +32,17 @@ function currentSlide(n) {
     showSlides();
 }
 
+console.log(document.getElementsByClassName("next"));
+
+for (let i = 0; i < document.getElementsByClassName("next").length; i++) {
+    document.getElementsByClassName("next").item(i).onclick = () => plusSlides(1);
+}
+for (let i = 0; i < document.getElementsByClassName("prev").length; i++) {
+    document.getElementsByClassName("prev").item(i).onclick = () => plusSlides(-1);
+}
 
 function showSlides() {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -30,6 +54,10 @@ function showSlides() {
     if (slideIndex > slides.length) {
         slideIndex = 1
     }
+    if (slideIndex <= 0) {
+        slideIndex = slides.length;
+    }
+    console.log(slideIndex);
 
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
